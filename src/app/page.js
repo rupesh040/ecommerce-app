539039c9f289bd8e55146cb364fd93958a5ccae9
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-08-07 21:22:34 +0530

    location

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,34 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import 'remixicon/fonts/remixicon.css';
 import Overlay from "./components/Overlay";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Account from "./components/Account";
 import Banner from "./components/Banner";
 import Carousel from "./components/Carousel";
 
 export default function Home() {
     const [overlay ,setOverlay] = useState(false);
+    const [longitude,setLongitude] = useState(null);
+    const [latitude,setLatitude] = useState(null);
+    const [geo,setGeo] = useState();
     const trueFalse = () =>{
         setOverlay(prevState => !prevState);
     }
+    useEffect(()=>{
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+            }),(err) => {alert("error")}
+        }
+        else{alert("error")}
+    },[]);
+    useEffect(()=>{
+        fetch(`https://geocode.maps.co/reverse?${latitude}&${longitude}&api_key=66b3975fa6904022042036iwv2f903c`)
+        .then(res => res.json())
+        .then(data => setGeo(data));
+    },[]);
+    console.log(geo);
   return (
   <>
 <Navbar btnOn={trueFalse}/>
