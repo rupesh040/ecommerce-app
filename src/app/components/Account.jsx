e9e8875d0e606b1f636c3adda00238462847e7f3
Rupesh kumar <112106535+rupesh040@users.noreply.github.com>
2024-08-11 21:58:51 +0530

    Update Account.jsx

--- src/app/components/Account.jsx
+++ src/app/components/Account.jsx
@@ -4,9 +4,13 @@ import React, { useState } from 'react'
 
 const Account = ({add,loc}) => {
     const [trues,setTrues] = useState(false);
+const click = () =>{ 
+ loc();
+setTrues(false);
+}
   return (
     <div className=' account w-[250px]  bg-white rounded-2xl absolute  mx-5 my-2 border-[1px] border-zinc-300 flex justify-between flex-col  z-20'>
-        {!trues?<h2 className='py-3 px-4 border-b-[1px] border-zinc-300 font-bold cursor-pointer' onClick={loc}>My Account</h2>:<h2 className='py-3 px-4 border-b-[1px] border-zinc-300 font-bold cursor-pointer'>My Address</h2>}
+        {!trues?<h2 className='py-3 px-4 border-b-[1px] border-zinc-300 font-bold cursor-pointer'>My Account</h2>:<h2 className='py-3 px-4 border-b-[1px] border-zinc-300 font-bold cursor-pointer'>My Address</h2>}
        {!trues? <div className=' flex flex-col'>
             <h2 className='hover:bg-zinc-100 py-2 px-4 cursor-pointer hover:px-6 transition-all ease-in-out'><i class="ri-user-line"></i> Profile</h2>
             <h2 className='hover:bg-zinc-100 py-2 px-4 cursor-pointer hover:px-6 transition-all ease-in-out'><i class="ri-draggable"></i> My order</h2>
@@ -14,7 +18,7 @@ const Account = ({add,loc}) => {
             <h2 className='hover:bg-zinc-100 py-2 px-4 cursor-pointer hover:px-6 transition-all ease-in-out'><i class="ri-settings-2-line"></i> Setting</h2>
 
         </div> : <h2 className='hover:bg-zinc-100 py-2 px-4 cursor-pointer hover:px-6 transition-all ease-in-out'>{add} </h2>}
-        {!trues?<h2 className=' logout py-2  px-4 border-t-[1px] border-zinc-300 font-bold cursor-pointer hover:bg-zinc-200  text-zinc-700 hover:px-6 transition-all ease-in-out '><i class="ri-logout-box-r-line mr-2"></i>Logout</h2>:<h2 className=' logout py-2  px-4 border-t-[1px] border-zinc-300 font-bold cursor-pointer hover:bg-zinc-200  text-zinc-700 hover:px-6 transition-all ease-in-out ' onClick={() => setTrues(false)}><i class="ri-logout-box-r-line mr-2"></i>Back</h2>}
+        {!trues?<h2 className=' logout py-2  px-4 border-t-[1px] border-zinc-300 font-bold cursor-pointer hover:bg-zinc-200  text-zinc-700 hover:px-6 transition-all ease-in-out '><i class="ri-logout-box-r-line mr-2"></i>Logout</h2>:<h2 className=' logout py-2  px-4 border-t-[1px] border-zinc-300 font-bold cursor-pointer hover:bg-zinc-200  text-zinc-700 hover:px-6 transition-all ease-in-out ' onClick={click}><i class="ri-logout-box-r-line mr-2"></i>Back</h2>}
     </div>
   )
 }
