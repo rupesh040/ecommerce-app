02712dd39abca73449d1e9ac7ccaec318e47c0f9
Rupesh kumar <112106535+rupesh040@users.noreply.github.com>
2024-08-11 21:35:57 +0530

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = ({btnOn,address}) => {
 
       <div className="cart  flex gap-5 items-center justify-center">{!logIn? <div className="login bg-black text-white px-[20px] py-[7px] rounded-lg cursor-pointer">Login</div> : <div className="login bg-black text-white px-[20px] py-[7px] rounded-lg cursor-pointer" onClick={trueFalse}>Account</div> } <i class="ri-shopping-cart-line text-[20px] bg-black text-white px-[20px] py-[10px] rounded-lg cursor-pointer"></i></div>
     </nav>
-    {!overlay?null:<Account add={address} location={btnOn} />}
+    {!overlay?null:<Account add={address} loc={btnOn} />}
     {!overlay?null:<Overlay btn={trueFalse} />}
     </>
   )
