ed4f69f61e2050e0eb8a166b70fb8b5e5318f658
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-08-15 11:42:19 +0530

    add some data

--- Data/category.js
+++ Data/category.js
@@ -6,37 +6,42 @@ category_name:"snacks",
 {
 img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-2_10.png",
 id:2,
-category_name:"dairy",
+category_name:"Dairy",
 },
 {
     img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-3_9.png",
     id:3,
-    category_name:"fruits and vegetables",
+    category_name:"Fruits and Vegetables",
     },
     {
         img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-6_5.png",
         id:4,
+        category_name:"Breakfast & instant food",
         },
         {
             img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-7_3.png",
             id:5,
+            category_name:"Sweet Tooth",
             },
 
                 {
                 img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-12.png",
                 id:6,
+                category_name:"Sauces & spreads",
                 },
                 {
                     img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-11.png",
                     id:7,
+                    category_name:"Masala,oil & More",
                     },
                     {
                         img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-9_3.png",
                         id:8,
+                        category_name:"Tea,Coffee & Health Drink",
                         },
                         {
                             img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-16.png",
                             id:9,
-                            category_name:"pharmacy",
+                            category_name:"Pharmacy",
                             },
 ]
--- Data/data.js
+++ Data/data.js
@@ -163,7 +163,7 @@ export const product_data = [
         weight:"50 ml",
         price:24,
         dis_price:25,
-        category:"pharmacy",
+        category:"Pharmacy",
         Key_Features:`Trusted Dettol Formula; Protects from 100 Illness Causing Germs.
         Use Dettol Hand Sanitizer to protect transference of germs from your hands anytime, anywhere without Soap or Water.
         Rinse Free and Non sticky.
@@ -184,7 +184,7 @@ export const product_data = [
         weight:"500 ml",
         price:34,
         dis_price:35,
-        category:"dairy",
+        category:"Dairy",
         Key_Features:`Wholesome and healthy
 Pasteurised milk
 Rich in calcium`,
@@ -205,7 +205,7 @@ Rich in calcium`,
         weight:"1 kg",
         price:40,
         dis_price:45,
-        category:"fruits and vegetables",
+        category:"Fruits and Vegetables",
         Key_Features:`Contains Vitamin C, Potassium, starch,.Potato helps in reducing inflammation, promote digestion and are good for skin`,
         Type:"vegetable",
         Country_of_Origin:"India",
@@ -222,7 +222,7 @@ Rich in calcium`,
         weight:"3 pieces",
         price:134,
         dis_price:150,
-        category:"fruits and vegetables",
+        category:"Fruits and Vegetables",
         Key_Features:`Rich source of Vitamin C, Contains Fibre Vitamin K, Amino acid.1. Vitamin C acts as a powerful antioxidant and also helps formation of collagen that is responsible for skin and hair health. Vitamin C helps in boosting immunity, keeping ailments at the bay.`,
         Type:"fruits",
         Country_of_Origin:"India",
@@ -232,5 +232,188 @@ Rich in calcium`,
         product_category:`Exotics `,
     },
 
+    {
+        id:13,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=540/app/assets/products/sliding_images/jpeg/8b000aad-94c7-401e-b8b6-3f5d7b0a9cc0.jpg?ts=1707806509",
+        name:`Maggi Special Masala Spicy Instant Noodles - Pack of 12`,
+        weight:"12x70 g",
+        price:192,
+        dis_price:210,
+        category:"Breakfast & instant food",
+        Key_Features:`Your favourite MAGGI now with an all new Special Masala
+An invigorating recipe of 20 spices and herbs with the goodness of iron
+Contains a blend of finely grounded and whole spices, roasted to perfection
+Each portion helps meet 15% of your daily iron requirement`,
+        Type:"Instant Noodles",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`MAGGI 2-minutes masala noodles have been a classic Indian snack for more than a decade. To take your favourite masala taste experience to the next level, introducing the new MAGGI special masala noodles, a recipe of 20 spices and herbs that provides a more redefined MAGGI masala experience.`,
+        product_category:`Noodles `,
+    },
+
+
+    {
+        id:14,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=1080/app/assets/products/sliding_images/jpeg/059e8a66-ab68-4e21-a150-3d2c30607ff2.jpg?ts=1723377105",
+        name:`Chef's Basket Instant Macaroni Pasta`,
+        weight:"850 g",
+        price:100,
+        dis_price:120,
+        category:"Breakfast & instant food",
+        Key_Features:`This Pasta has been crafted from the finest quality ingredients.
+It satisfies your cravings for authentic Italian Pasta.
+Its perfect shape and size allow any sauce or dressing to seep well.
+Perfect any time snack, cook with rich olive oil and lots of fresh vegetables.
+Perfect with pesto and tomato-based sauces.`,
+        Type:"Macaroni",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Make delicious macaroni at home in minutes with Weikfield Chef's Basket Pasta Macaroni. The macaroni is made from 100% Suji. It is rich in Protein and Fibre, which promote slow release of energy, keeping you active and satiated for a long period. It is non-sticky and has just the right bite, making it perfect for a scrumptious snack or meal.`,
+        product_category:`Pasta  `,
+    },
+
+    {
+        id:15,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=900/app/assets/products/sliding_images/jpeg/8937ab2e-eda9-43e2-a261-d550230eb8b2.jpg?ts=1723375142",
+        name:`Knorr Hot & Sour Vegetable Soup`,
+        weight:"41 g",
+        price:60,
+        dis_price:89,
+        category:"Breakfast & instant food",
+        Key_Features:`Made with 100% real vegetables.
+Contains no added preservatives.
+Ready to eat in 3 simple steps.`,
+        Type:"Vegetable Soup",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Make delicious soup at home in minutes with Weikfield Chef's Basket Pasta Macaroni. The soup is made from 100% Suji. It is rich in Protein and Fibre, which promote slow release of energy, keeping you active and satiated for a long period. It is non-sticky and has just the right bite, making it perfect for a scrumptious snack or meal.`,
+        product_category:`Soup  `,
+    },
+    {
+        id:16,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=1080/app/assets/products/sliding_images/jpeg/b56488f8-430e-4a82-9aa2-03d4bde655a5.jpg?ts=1723098534",
+        name:`Yoga Bar Chocolate Chunk Nut Multigrain Protein Bar (38 g)`,
+        weight:"38 g",
+        price:38,
+        dis_price:49,
+        category:"Breakfast & instant food",
+        Key_Features:`Chocolate energy`,
+        Type:"Macaroni",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Loaded with the goodness of whole grains, dried fruits, nuts, seeds, and cocoa
+Makes the perfect grab-and-go snack for instant energy
+Rich in Fibre`,
+        product_category:` Energy Bars  `,
+    },
+
+    {
+        id:17,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=900/app/assets/products/sliding_images/jpeg/cb0b6908-856f-4a66-b3c5-1ee495445f91.jpg?ts=1713170916",
+        name:`Nestle KitKat Minis Chocolate Coated Wafer Bar`,
+        weight:"123.2 g",
+        price:112,
+        dis_price:120,
+        category:"Sweet Tooth",
+        Key_Features:`Delight your taste buds with your favourite KITKAT wafer bar in a mini-size.
+Indulge in the delicious crispy & chocolaty wafer bar and make your mini breaks delightful.
+This pack is perfect for enjoying at home or for passing around with your friends and family.
+Carry a KITKAT Mini in your pocket, bag or lunchbox and enjoy it anytime, anywhere.
+Pack contains 18 units of 1 Finger breaks inside.`,
+        Type:"Chocolates ",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`HAVE A BREAK, HAVE A KITKAT!
+Share your favourite treats with your favourite people and indulge in the deliciousness of NESTLE KITKAT Minis! This share bag contains 18 units of KITKAT Minis, making it perfect for stocking up or passing around with friends and family. The iconic taste and texture of KitKat , combined with the crispiness of the wafer bar, will delight your senses and make your breaks delicious! Outer Packing may vary depending on stock availability`,
+        product_category:`Chocolates  `,
+    },
+
+
+    {
+        id:18,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/b5528701-6cc1-4094-9c9a-ce8c5c147852.jpg?ts=1722517782",
+        name:`Cadbury Celebrations Rich Dry Fruit Collection Chocolate Gift Pack`,
+        weight:"177 g",
+        price:440,
+        dis_price:499,
+        category:"Sweet Tooth",
+        Key_Features:`-A perfect gift for Rakhi to show love & admiration to your loved ones
+-This gift pack contains Almonds, Cashews & Raisins enrobed in rich chocolate
+-Celebrate this Rakhi by gifting Cadbury Celebrations to your friends and family.`,
+        Type:"Chocolates ",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Kuch meetha ho jaaye with Cadbury Celebrations. Filled with the goodness of Cadbury chocolates, like Cadbury Dairy Milk, the perfect gift for those whose presence makes every occasion perfect. Made with love, a chocolate box meant for gifting on every occasion- Rakhi, Birthday, or Diwali. This assorted chocolates gift pack contains Almonds, Cashews & Raisins enrobed in rich chocolate.`,
+        product_category:`Chocolates  `,
+    },
+
+    {
+        id:19,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/images/products/full_screen/pro_479704.jpg?ts=1685984544",
+        name:`NVeeba Chef's Special Tomato Ketchup`,
+        weight:"900 g",
+        price:75,
+        dis_price:99,
+        category:"Sauces & spreads",
+        Key_Features:`Made from rich, high quality tomatoes.
+A perfect dip for snacks and sandwiches.
+Tangy taste that goes well with every snack.`,
+        Type:"Tomato Ketchup ",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Made from rich, high quality tomatoes.
+A perfect dip for snacks and sandwiches.
+Tangy taste that goes well with every snack.`,
+        product_category:`Tomato Ketchup  `,
+    },
+    {
+        id:20,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/0efd1a16-11f6-4d48-8fda-a99d99b82e86.jpg?ts=1707312307",
+        name:`Fortune Soya Health Refined Soyabean Oil`,
+        weight:"1 L",
+        price:114,
+        dis_price:120,
+        category:"Masala,oil & More",
+        Key_Features:`India’s No.1 Oil - Healthy Refined oil for your Family,
+Fortified with the goodness of Vitamin A and Vitamin D,
+Rich in Omega 3 – helps keep Heart healthy,
+Contains PUFA – which helps in reducing cholesterol levels,
+Helps maintain Strong Bones,
+Adds extra Flavour to your favourite food.`,
+        Type:"Soyabean Oil ",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Fortune soya health is certified as India's No. 1 cooking oil brand. It is fortified with Vitamin A & Vitamin D which helps bosting immunity.
+Fortune Soya Health Oil contains PUFA, which helps in reducing cholesterol levels.
+Your everyday meals prepared with Fortune soya health oil not only tastes better, but also make your bones stronger and heart healthier.`,
+        product_category:`oil  `,
+    },
+    {
+        id:21,
+        img:"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=85,metadata=none,w=480,h=480/app/assets/products/large_images/jpeg/147192a4-9eb8-42e1-81b4-3494d578465f.jpg?ts=1722662746",
+        name:`Brooke Bond Taj Mahal Tea 250 g`,
+        weight:"250 g",
+        price:170,
+        dis_price:199,
+        category:"Tea,Coffee & Health Drink",
+        Key_Features:`Tasted, graded and perfectly blended at the Brooke bond Tea Excellence Centre
+Comes in a unique flavour lock pack
+Invites you to cheer your senses
+Perfect balance of strength and flavour`,
+        Type:"Leaf Tea ",
+        Country_of_Origin:"India",
+        Return_Policy:`This Item is non-returnable. For a damaged, defective, incorrect or expired item, you can request a replacement within 72 hours of delivery.
+        In case of an incorrect item, you may raise a replacement or return request only if the item is sealed/ unopened/ unused and in original condition.`,
+        Description:`Brookbond Taj Mahal Tea is a blend of expertly chosen tea leaves that bring a strong aroma and great taste together. Strong and refreshing, this tea is made from selected tea leaves that are blended to perfection to impart a natural goodness. This tea blend is tasted, graded and perfectly blended at the Brooke Bond Tea Excellence Centre to keep the authentic taste intact. The pack contains a unique flavour lock system to keep the aroma intact. Imparts a pleasing aura that cheers you up from within. Perfect balance of strength and flavour.`,
+        product_category:`Leaf Tea  `,
+    },
 
 ]
